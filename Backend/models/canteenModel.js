3ab315a1b83d3e401e566b8fde71f517715042f5
ishaan <ishaan22parashar@gmail.com>
2026-07-27 10:45:13 +0530

    fixed canteenModel query errors

--- Backend/models/canteenModel.js
+++ Backend/models/canteenModel.js
@@ -2,30 +2,30 @@ import pool from "../config/db.js";
 
 export async function createCanteen(canteen_name, canteen_location, opening_time, closing_time) {
     const sql = "INSERT INTO canteens (canteen_name, canteen_location, opening_time, closing_time) VALUES ($1,$2,$3, $4)";
-    const result = await pool(sql, [canteen_name, canteen_location, opening_time, closing_time]);
+    const result = await pool.query(sql, [canteen_name, canteen_location, opening_time, closing_time]);
     return result;
 }
 
 export async function updateCanteen(canteen_id, fields, values) {
     const sql = `UPDATE canteens SET ${fields.join(", ")} WHERE canteen_id = ${fields.lenght + 1}`;
-    const result = await pool(sql, [...values, canteen_id]);
+    const result = await pool.query(sql, [...values, canteen_id]);
     return result;
 }
 
 export async function getCanteen(canteen_id) {
     const sql = `SELECT * FROM canteens WHERE canteen_id = $1`;
-    const result = await pool(sql, [canteen_id]);
+    const result = await pool.query(sql, [canteen_id]);
     return result;
 }
 
 export async function deleteCanteen(canteen_id) {
     const sql = "DELETE FROM canteens WHERE canteen_id = $1";
-    const result = await pool(sql, [canteen_id]);
+    const result = await pool.query(sql, [canteen_id]);
     return result;
 }
 
 export async function openCanteen(canteen_id, is_open) {
     const sql = "UPDATE canteens SET is_open = $1 WHERE canteen_id = $2";
-    const result = await pool(sql, [canteen_id, is_open]);
+    const result = await pool.query(sql, [canteen_id, is_open]);
     return result;
 }
