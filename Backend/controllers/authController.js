fb655d640363d6531f19af15fddd58efddb6ca2c
ishaan <ishaan22parashar@gmail.com>
2026-08-05 21:46:35 +0530

    changed refreshtokens to not use cookies

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -5,7 +5,7 @@ import {storeToken, findUser, deleteToken} from "../models/refreshTokenModel.js"
 import {findUserByPhoneNumber} from "../models/userModel.js";
 
 // used in the app
-export async function logIn(req, res) {
+export async function logIn(req, res, next) {
     
     const {phone_number, password} = req.body;
 
@@ -52,8 +52,8 @@ export async function logIn(req, res) {
             
             const token_stored = await storeToken(UUID, token_hash, user_detail.user_id, 7);
             
-            res.cookie('jwt', refreshToken, {httpOnly:true, maxAge: 7 * 24 * 60 * 60 *1000});
-            res.status(200).json({accessToken});
+            // res.cookie('jwt', refreshToken, {httpOnly:true, maxAge: 7 * 24 * 60 * 60 *1000});
+            res.status(200).json({"accessToken" : accessToken, "refreshToken": refreshToken});
             
         }        
         
@@ -68,79 +68,82 @@ export async function logIn(req, res) {
 }
 
 // used in the app
-export async function logOut(req, res) {
-    const cookie = req.cookies;
+export async function logOut(req, res, next) {
+    try {
 
-    // user not logged in
-    if (!cookie?.jwt) {
-        console.log('no cookie');
+        const refreshToken = req.body.refreshToken;
         
+        // user not logged in
+        if (!refreshToken) {        
+            return res.sendStatus(204);
+        }
+        
+        let payload = null;
+        
+        try {
+            payload = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET);
+        }
+        // cookie has been tampered with, couldn't verify it
+        catch (err) {
+            console.log('tampered');
+            
+            return res.sendStatus(203)
+        }
+        
+        const [user] = await findUser(payload.jti);
+        
+        // cookie has expired, couldn't find it in the database
+        if (!user) {
+            res.clearCookie('jwt', {httpOnly:true, maxAge: 7 * 24 * 60 * 60 *1000});
+            console.log('expired');
+            
+            return res.sendStatus(203)
+        }
+        
+        const result = await deleteToken(payload.jti);
+        
+        // res.clearCookie('jwt', {httpOnly:true, maxAge:7*24*60*60*1000});
         return res.sendStatus(204);
     }
-
-    let payload = null;
-    
-    try {
-        payload = jwt.verify(cookie.jwt, process.env.REFRESH_TOKEN_SECRET);
-    }
-    // cookie has been tampered with, couldn't verify it
     catch (err) {
-        res.clearCookie('jwt', {httpOnly:true, maxAge: 7 * 24 * 60 * 60 *1000});
-        console.log('tampered');
-        
-        return res.sendStatus(203)
+        next(err);
     }
-
-    const [user] = await findUser(payload.jti);
-
-    // cookie has expired, couldn't find it in the database
-    if (!user) {
-        res.clearCookie('jwt', {httpOnly:true, maxAge: 7 * 24 * 60 * 60 *1000});
-        console.log('expired');
-        
-        return res.sendStatus(203)
-    }
-
-    const result = await deleteToken(payload.jti);
-
-    res.clearCookie('jwt', {httpOnly:true, maxAge:7*24*60*60*1000});
-    console.log('deleted');
-    return res.sendStatus(204);
 }
 
 // used in the app
-export async function createNewToken(req, res) {
-    const cookie = req.cookies;
-    
-    if (!cookie?.jwt) {
-        return res.status(403).json({message: "no cookie"});
-    }
-
-    const refreshToken = cookie.jwt;
-
-    let payload = null;
-
+export async function createNewToken(req, res, next) {
     try {
-        payload = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET)
 
-    }
-    catch (err) {
-        return res.status(403).json({message:err.message});
-    }
-
-    const [user] = await findUser(payload.jti);    
-
-    if (!user) {
-        return res.status(403).json({message: "no user"});
-    }
-
-    const match = compare(refreshToken, user.token_hash);
-
-    if (!match) {
-        return res.status(403);
-    }
-
-    const accessToken = jwt.sign({
+        const refreshToken = req.body.refreshToken;
+        
+        // user not logged in
+        if (!refreshToken) {        
+            return res.sendStatus(204);
+        }
+        
+        let payload = null;
+        
+        try {
+            payload = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET)
+            
+        }
+        catch (err) {
+            return res.status(403).json({message:err.message});
+        }
+        
+        const [user] = await findUser(payload.jti);    
+        
+        if (!user) {
+            return res.status(403).json({message: "no user"});
+        }
+        
+        const match = compare(refreshToken, user.token_hash);
+        
+        if (!match) {
+            return res.status(403);
+        }
+        
+        const accessToken = jwt.sign({
                 user_id: user.user_id,
                 canteen_id: user.canteen_id,
                 role: user.role
@@ -149,6 +152,10 @@ export async function createNewToken(req, res) {
             { expiresIn:'15m'}
         );
     return res.status(201).json({accessToken});
+    }
+    catch (err) {
+        next(err);
+    }
 }
 
 
